100e3e47d037ed91d838dde7cdc0b88e2f7c4589
noahvers <95831760+noahvers@users.noreply.github.com>
2025-04-09 22:26:34 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ import poker.HandEvaluator;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		/**
 		Deck deck = new Deck();
 		System.out.println(deck.draw().toString());
